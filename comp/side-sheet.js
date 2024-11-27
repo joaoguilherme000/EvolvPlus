5c55e87d7f5601a838a5388991c50febe58fc068
joaoguilherme000 <joaoguilhermehig@gmail.com>
2024-11-27 00:32:50 -0300

    pagina de produtos responsiva e side sheet melhorado

--- comp/side-sheet.js
+++ comp/side-sheet.js
@@ -4,10 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const sideSheet = document.getElementById('sideSheet');
 
   menuButton.addEventListener('click', () => {
+    sideSheet.style.display = 'flex';
+    sideSheet.style.flexDirection = 'column';
     sideSheet.style.right = '0';
   });
 
   closeButton.addEventListener('click', () => {
     sideSheet.style.right = '-100%';
+
+    setTimeout(() => {
+      sideSheet.style.display = 'none'; // Oculta após a transição
+    }, 500);
   });
 });
